fc6a11193b1abaac60544c0e49adcd4c4dcca7fd
“Weining Lv” <18500212279@163.com>
2018-11-13 16:24:28 +0800

    fix github fix test
    fix github fix test

--- News/Main/Controller/AppDelegate.swift
+++ News/Main/Controller/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        //这是GitHub客户端测试上传
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         
         window?.rootViewController = MyTabBarController()
